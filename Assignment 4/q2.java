96855578414abcd0e3626893c0b61e0dd5c7789f
lainfec <54847932+lainfec@users.noreply.github.com>
2020-10-12 18:23:22 +0530

    Add files via upload

---
+++ Assignment 4/q2.java
@@ -0,0 +1,15 @@
+class q2
+{
+    public static void main(String[] args)
+    {
+        int num = -1;
+        byte b = (byte)num;
+        int n = b;
+        char c = (char)b, c1 = (char)n;
+        int final_num = c;
+        System.out.println("Initial integer: "+ num); // -1
+        System.out.println("Byte: "+ b + " " + n + " " + c1); // -1
+        System.out.println("Character: "+ c); // a rectangle char
+        System.out.println("Final integer: "+ final_num); // 65535
+    }
+}
